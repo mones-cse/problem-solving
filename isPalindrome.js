649638cd8946713ee0bb0465648b1c239e27c1be
mones <mones.cse@gmail.com>
2024-04-22 02:02:42 +0600

    isPalindrome wip

---
+++ isPalindrome.js
@@ -0,0 +1,8 @@
+var isPalindrome = function (s) {};
+
+const runIsPalindrome = () => {
+  console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+  console.log(isPalindrome("race a car")); // false
+};
+
+export default runIsPalindrome;
