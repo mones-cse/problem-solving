38e69944ef862d4d41b32f387567a5c09f142da3
mones <mones.cse@gmail.com>
2024-04-23 17:51:17 +0600

    isPalindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,4 +1,12 @@
-var isPalindrome = function (s) {};
+var isPalindrome = function (s) {
+  const str = s
+    .split("")
+    .filter((char) => char.match(/[a-zA-Z0-9]/))
+    .join("")
+    .toLowerCase();
+  const reversedStr = str.split("").reverse().join("");
+  return str === reversedStr;
+};
 
 const runIsPalindrome = () => {
   console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
