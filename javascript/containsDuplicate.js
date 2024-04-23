972f60a5004c065167c2348fd040cb530d4ecf79
mones <mones.cse@gmail.com>
2024-04-24 01:04:12 +0600

    containsDuplicate

--- javascript/containsDuplicate.js
+++ javascript/containsDuplicate.js
@@ -1,16 +1,23 @@
 // 217. Contains Duplicate
 // https://leetcode.com/problems/contains-duplicate/
 
-const containsDuplicate = function (nums) {
-  let temp = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (temp[nums[i]] == true) {
-      return true;
-    } else {
-      temp[nums[i]] = true;
-    }
-  }
-  return false;
+// solution 1
+// const containsDuplicate = function (nums) {
+//   let temp = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (temp[nums[i]] == true) {
+//       return true;
+//     } else {
+//       temp[nums[i]] = true;
+//     }
+//   }
+//   return false;
+// };
+
+// solution 2
+var containsDuplicate = function (nums) {
+  const s = new Set(nums);
+  return s.size !== nums.length;
 };
 
 const runContainsDuplicate = () => {
